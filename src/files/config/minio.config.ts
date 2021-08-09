13466631e8f8ccdf228a596bcc567b3e872071d1
Ines Quass <ines.q.311@gmail.com>
2021-08-09 19:14:08 +0200

    refactor minio config

---
+++ src/files/config/minio.config.ts
@@ -0,0 +1,29 @@
+const bucketName = 'test-bucket';
+
+export const policy = {
+  Version: '2012-10-17',
+  Statement: [
+    {
+      Effect: 'Allow',
+      Principal: {
+        AWS: ['*'],
+      },
+      Action: ['s3:ListBucketMultipartUploads', 's3:GetBucketLocation', 's3:ListBucket'],
+      Resource: ['arn:aws:s3:::' + bucketName],
+    },
+    {
+      Effect: 'Allow',
+      Principal: {
+        AWS: ['*'],
+      },
+      Action: [
+        's3:PutObject',
+        's3:AbortMultipartUpload',
+        's3:DeleteObject',
+        's3:GetObject',
+        's3:ListMultipartUploadParts',
+      ],
+      Resource: ['arn:aws:s3:::' + bucketName + '/*'],
+    },
+  ],
+};
--- src/minio-client/minio-client.service.ts
+++ src/minio-client/minio-client.service.ts
@@ -3,41 +3,14 @@ import { Injectable, Logger } from '@nestjs/common';
 import { MinioService } from 'nestjs-minio-client';
 import { BufferedFile, FileData, FileMetadata } from '../files/file.model';
 import * as crypto from 'crypto';
+import { policy } from '../files/config/minio.config';
 
 @Injectable()
 export class MinioClientService {
   constructor(private readonly minio: MinioService) {
     this.logger = new Logger('MinioService');
 
-    const policy = {
-      Version: '2012-10-17',
-      Statement: [
-        {
-          Effect: 'Allow',
-          Principal: {
-            AWS: ['*'],
-          },
-          Action: ['s3:ListBucketMultipartUploads', 's3:GetBucketLocation', 's3:ListBucket'],
-          Resource: ['arn:aws:s3:::' + process.env.MINIO_BUCKET_NAME], // Change this according to your bucket name
-        },
-        {
-          Effect: 'Allow',
-          Principal: {
-            AWS: ['*'],
-          },
-          Action: [
-            's3:PutObject',
-            's3:AbortMultipartUpload',
-            's3:DeleteObject',
-            's3:GetObject',
-            's3:ListMultipartUploadParts',
-          ],
-          Resource: ['arn:aws:s3:::' + process.env.MINIO_BUCKET_NAME + '/*'], // Change this according to your bucket name
-        },
-      ],
-    };
-
-    this.client.setBucketPolicy(
+    this.minio.client.setBucketPolicy(
       process.env.MINIO_BUCKET_NAME,
       JSON.stringify(policy),
       function (err) {
@@ -51,9 +24,9 @@ export class MinioClientService {
   private readonly logger: Logger;
   private readonly bucketName = process.env.MINIO_BUCKET_NAME;
 
-  public get client() {
-    return this.minio.client;
-  }
+  // public get client() {
+  //   return this.minio.client;
+  // }
 
   public async upload(
     file: BufferedFile,
@@ -72,7 +45,6 @@ export class MinioClientService {
     if (objectName) {
       existingFile = await this.get(objectName);
       existingFileMetaData = existingFile.metaData;
-      console.log('File found: ', existingFile);
     }
 
     if (objectName) {
@@ -97,7 +69,7 @@ export class MinioClientService {
       modifieddate: new Date(),
     };
 
-    this.client.putObject(bucketName, fileName, file.buffer, metaData, function (err, res) {
+    this.minio.client.putObject(bucketName, fileName, file.buffer, metaData, function (err, res) {
       if (err) {
         throw new HttpException('Error uploading file.', HttpStatus.BAD_REQUEST);
       }
@@ -107,26 +79,29 @@ export class MinioClientService {
   }
 
   public async delete(objectName: string, bucketName: string = this.bucketName) {
-    this.client.removeObject(bucketName, objectName, function (err, res) {
+    this.minio.client.removeObject(bucketName, objectName, function (err, res) {
       if (err) {
         throw new HttpException('An error occured when deleting.', HttpStatus.BAD_REQUEST);
       }
     });
   }
 
+  //TODO: improve error handling here
   public async get(objectName: string, bucketName: string = this.bucketName): Promise<FileData> {
-    return this.client.statObject(bucketName, objectName);
+    return this.minio.client.statObject(bucketName, objectName);
   }
 
   //TODO: this throws an error when there is exactly 1 file in the bucket
   public async getAll(bucketName: string = this.bucketName): Promise<FileData[]> {
     const fileDataArray: FileData[] = await new Promise((resolve, reject) => {
       const arr: FileData[] = [];
-      const stream = this.client.listObjects(bucketName);
+      const stream = this.minio.client.listObjects(bucketName);
       stream.on('data', (obj) => {
         arr.push(obj);
       });
-      stream.on('error', reject);
+      stream.on('error', (err) => {
+        reject(err);
+      });
       stream.on('end', () => {
         resolve(arr);
       });
